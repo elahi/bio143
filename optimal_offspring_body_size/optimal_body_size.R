e3d3b112bbfbc6b0f267ebfe007694cf797eb4a2
Robin Elahi <elahi.robin@gmail.com>
2022-01-09 11:47:33 -0800

    finished body size problem

--- optimal_offspring_body_size/optimal_body_size.R
+++ optimal_offspring_body_size/optimal_body_size.R
@@ -83,4 +83,39 @@ x_opt
 
 ##### USING R FOR SYMBOLIC CALCULATION #####
 
+# Translate function into an R expression
+lambda_expr <- expression(alpha * ((x - gamma)/(1 + beta * (x - gamma))) * delta / x)
+lambda_expr
+
+# Ask R to compute derivative
+lambda_der <- D(lambda_expr, "x")
+lambda_der
+
+# Write function to evaluate the derivative
+f_lambda_der <- function(x) eval(lambda_der)
+
+# Is the derivative of the function at x_opt zero?
+f_lambda_der(x_opt) # 
+
+# Compute and plot the derivative of lambda as a function of body size x
+lambda_der_vec <- sapply(size_vec, f_lambda_der)
+plot(lambda_vec ~ size_vec, cex = 0.5, col = "blue")
+abline(v = c(gamma, 20 * gamma), h = 0, lty = 3)
+abline(v = x_opt, col = "red")
+
 ##### ALL THREE FUNCTIONS ON THE SAME PLOT #####
+
+par(mfrow =c(1,1), mar=c(6,4,1,8)) # set margin size to accommodate two y axes on right 
+plot(sigma_vec ~ size_vec, type="l", cex=0.5, col="green", lwd=2, xlim=c(0,10*gamma))
+leg.txt <- c("Sigma", "Offspring number", "Lambda")
+legend(0.0004, 0.000004, leg.txt, col=c("green", "red", "blue"), lty=1, lwd=2, bty="n")
+
+par(new=T) #par adds new data to prior plot without overwriting it 
+plot(offspring_vec ~ size_vec, axes = F, cex=0.5, col="red", ylab="", lwd=2, type="l", xlim=c(0,10*gamma))
+axis(4) #position 4 is the right side of the quartz window
+mtext("Offspring number", side = 4, line=-2) # 'line' adjusts position of axis label
+par(new=T) #par adds new data to prior plot without overwriting it 
+plot(lambda_vec ~ size_vec, axes = F, cex=0.5, col="blue", type = 'l', ylab="", lwd=2, xlim=c(0,10*gamma))
+axis(4, line=4) #position 4 is the right side of the quartz window
+mtext("Lambda", side = 4, line=6)
+abline(v=x_opt, col="black", lty=2)
